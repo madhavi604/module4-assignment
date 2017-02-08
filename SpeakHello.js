c397d6ebcf3f13b9e3fef0dd6c03b284d17e7ad5
Madhavi Joshi <Madhavi Joshi>
2017-02-08 11:14:43 -0500

    solution

--- SpeakHello.js
+++ SpeakHello.js
@@ -16,7 +16,6 @@ var speakWord = "Hello";
 helloSpeaker.speak = function speak(name) {
   console.log(speakWord + " " + name);
 }
-
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
